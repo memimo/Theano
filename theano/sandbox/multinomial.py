ab0df2ee8568bf506f1c9ab951b61e9945f8aef1
Mehdi Mirza <memirzamo@gmail.com>
2013-01-30 15:24:53 -0500

    rng multinomial grad returns zerp now

--- theano/sandbox/multinomial.py
+++ theano/sandbox/multinomial.py
@@ -5,6 +5,7 @@ from theano import Op, Apply
 import theano.tensor as T
 from theano.gof import local_optimizer
 from theano.gof.python25 import any
+from theano.gradient import grad_undefined
 
 from theano.sandbox.cuda import cuda_available, GpuOp
 if cuda_available:
@@ -44,7 +45,7 @@ class MultinomialFromUniform(Op):
     def grad(self, ins, outgrads):
         pvals, unis = ins
         (gz,) = outgrads
-        return [None, None]
+        return [T.zeros_like(x) for x in ins]
 
     def c_code_cache_version(self):
         return (5,)
